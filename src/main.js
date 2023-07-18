e37681419f2ca04d28485404812d42f30f95b364
tamires_camargo <tamires.camargo.soares@gmail.com>
2023-07-18 19:25:18 -0300

    ajustes finais

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        console.log(window.scrollY);
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -17,6 +30,16 @@ document.addEventListener('DOMContentLoaded', function() {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
